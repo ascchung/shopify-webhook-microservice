0ba433a55e1364a8208e197796d7d3aeb3a7d9a4
Andrew Chung <ascchung@gmail.com>
2024-08-10 18:49:49 -0700

    Add root route to handle GET /

--- backend/routes/webhook-routes.js
+++ backend/routes/webhook-routes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { handleCustomerUpdate } = require('../controllers/webhook-controllers');
+const express = require("express");
+const { handleCustomerUpdate } = require("../controllers/webhook-controllers");
 
 const router = express.Router();
 
-router.post('/customers/update', handleCustomerUpdate);
+router.post("/customers/update", handleCustomerUpdate);
 
 module.exports = router;
--- backend/utils/app.js
+++ backend/utils/app.js
@@ -1,20 +1,27 @@
-const express = require('express');
-require('dotenv').config();
-const { rawBodySaver, verifyWebhook } = require('../utils/verify-webhook');
-const webhookRoutes = require('../routes/webhook-routes');
+const express = require("express");
+require("dotenv").config();
+const { rawBodySaver, verifyWebhook } = require("../utils/verify-webhook");
+const webhookRoutes = require("../routes/webhook-routes");
 
 const app = express();
 app.use(express.json({ verify: rawBodySaver }));
 
 // Routes
-app.use('/api/webhooks', verifyWebhook, webhookRoutes);
+app.use("/api/webhooks", verifyWebhook, webhookRoutes);
+
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Hello welcome to my deployed app");
+});
 
 // Error handling middleware
 app.use((error, req, res, next) => {
   if (res.headersSent) {
     return next(error);
   }
-  res.status(error.code || 500).json({ message: error.message || 'An unknown error occurred.' });
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "An unknown error occurred." });
 });
 
 module.exports = app;
