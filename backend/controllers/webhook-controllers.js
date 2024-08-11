49e2805be9bf5064a30f4e21b03a4ec7aa284e3c
Andrew Chung <ascchung@gmail.com>
2024-08-11 01:09:16 -0700

    updated controller

--- backend/controllers/webhook-controllers.js
+++ backend/controllers/webhook-controllers.js
@@ -3,21 +3,16 @@ const HttpError = require("../models/http-error");
 require("dotenv").config();
 
 const handleCustomerUpdate = async (req, res, next) => {
-  const { customer_id, email, email_marketing_consent } = req.body;
+  const { id: customer_id, email, email_marketing_consent } = req.body;
 
-  console.log("Received payload:", req.body);
+  // Check if all necessary data is present
+  if (!customer_id || !email || !email_marketing_consent) {
+    return next(new HttpError("Invalid payload structure", 400));
+  }
 
   try {
-    // Check if necessary data is present
-    if (!customer_id || !email || !email_marketing_consent) {
-      console.error("Missing customer ID, email, or marketing consent data.");
-      return next(new HttpError("Invalid payload structure", 400));
-    }
-
     // Determine if the customer should be unsubscribed
     if (email_marketing_consent.state === "subscribed") {
-      console.log(`Customer with ID ${customer_id} is currently subscribed.`);
-
       // Update the customer to "not_subscribed"
       const client = axios.create({
         baseURL: `https://${process.env.SHOPIFY_STORE_URL}/admin/api/2024-07`,
@@ -38,18 +33,15 @@ const handleCustomerUpdate = async (req, res, next) => {
         }
       );
 
-      console.log("Customer updated to not_subscribed:", updateResponse.data);
       return res
         .status(200)
         .json({ message: "Customer unsubscribed successfully" });
     } else {
-      console.log(`Customer with ID ${customer_id} is not subscribed.`);
       return res
         .status(200)
         .json({ message: "Customer is not subscribed, no changes made." });
     }
   } catch (error) {
-    console.error("Error processing webhook:", error);
     return next(new HttpError("Internal server error", 500));
   }
 };
